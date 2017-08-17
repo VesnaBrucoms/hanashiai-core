61f40b6d59966ec4fe5526aeb034fee12781ab8c
Trevalyan <etstevens60@gmail.com>
2017-08-17 22:48:44 +0100

    Added sub ordering
    * Added more try blocks
    * Added ENV's to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,6 @@ RUN pip3 install /tmp/hanashiai_core/.
 RUN rm -rf /tmp/hanashiai_core
 
 COPY ./misc/ /opt/tests/
+
+ENV CLIENT_ID id
+ENV CLIENT_SECRET secret
--- hanashiai/core/interface.py
+++ hanashiai/core/interface.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import platform
+import re
 
 import praw
 from prawcore.exceptions import ResponseException
@@ -33,12 +34,12 @@ class RedditPort():
                                        user_agent=self._user_agent)
             logging.info('Connected to reddit with user agent: %s',
                          self._user_agent)
-
-            self._subreddit = self._reddit.subreddit(subreddit)
-            logging.info('Retreived subreddit: %s', subreddit)
         except ResponseException as exception:
-            logging.error('When connecting returned HTTP response: %s',
-                          exception.response)
+            logging.error('Connecting returned HTTP response: %s',
+                          exception.response.status_code)
+
+        self._subreddit = self._reddit.subreddit(subreddit)
+        logging.info('Set subreddit to: %s', subreddit)
 
     def search(self, query):
         """Search the subreddit with the query.
@@ -47,11 +48,23 @@ class RedditPort():
             query (str): Search query
         """
         submissions = []
-        for result in self._subreddit.search(query):
-            submissions.append(result)
+        try:
+            for result in self._subreddit.search(query):
+                submissions.append(result)
+        except ResponseException as exception:
+            http_code = exception.response.status_code
+            err_msg = 'Search with query "{}" returned HTTP {}'.format(query,
+                                                                       http_code)
+            if http_code == 401:
+                logging.error('%s, you are unauthorised', err_msg)
+            elif http_code == 302:
+                logging.error('%s, subreddit may not exist', err_msg)
+            else:
+                logging.error(err_msg)
 
         filtered_subs = self._filter_submissions(submissions)
-        sorted_subs = self._sort_submissions(filtered_subs)
+        ordered_subs = self._order_submissions(filtered_subs)
+        sorted_subs = self._sort_submissions(ordered_subs)
 
         return sorted_subs
 
@@ -74,6 +87,10 @@ class RedditPort():
 
         return filtered_subs
 
+    def _order_submissions(self, submissions):
+        submissions.sort(key=_order_key)
+        return submissions
+
     def _sort_submissions(self, submissions):
         sorted_subs = {'discussions': [], 'rewatches': []}
         for sub in submissions:
@@ -84,3 +101,16 @@ class RedditPort():
                 sorted_subs['discussions'].append(sub)
 
         return sorted_subs
+
+
+def _order_key(value):
+    print(value.title)
+    regex = r'(((E|e)pisode|(E|e)p|OVA|ova)\s)([0-9]{1,4})(\s(D|d)iscussion)*'
+    match_groups = re.search(regex, value.title)
+    print(match_groups)
+    # print(match_groups.group[3])
+
+    if match_groups is None:
+        return 1000
+    else:
+        return int(match_groups.groups()[4])
