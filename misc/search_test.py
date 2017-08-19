a5b91fb37317a9992b4e8d4d0da9290ee003351b
Trevalyan <etstevens60@gmail.com>
2017-08-19 22:57:29 +0100

    Added getting top-level comments
    * Added RedditResponseError
    * Improved some error logging

--- misc/search_test.py
+++ misc/search_test.py
@@ -1,3 +1,4 @@
+import time
 from hanashiai.core.interface import RedditPort
 
 
@@ -7,10 +8,19 @@ reddit = RedditPort(name='Hanashiai',
 
 reddit.connect('anime')
 results = reddit.search('knights of sidonia discussion')
-print('Discussions\n')
+print('\nDiscussions:')
 for dis in results['discussions']:
     print(dis.title)
 
-print('Rewatches\n')
+print('\nRewatches:')
 for dis in results['rewatches']:
     print(dis.title)
+
+comments = reddit.get_submission_comments(results['discussions'][0])
+print(results['discussions'][0].title)
+count = 0
+for comment in comments:
+    print(comment.body)
+    count = count + 1
+    if count == 10:
+        break
